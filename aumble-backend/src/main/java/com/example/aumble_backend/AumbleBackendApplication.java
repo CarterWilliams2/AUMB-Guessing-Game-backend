5095d1c1e2efa26e911c9753d289d5dcbcfc41dd
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2025-09-28 11:59:57 -0500

    unzip spring intial

---
+++ aumble-backend/src/main/java/com/example/aumble_backend/AumbleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.aumble_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AumbleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AumbleBackendApplication.class, args);
+	}
+
+}
---
+++ aumble-backend/src/test/java/com/example/aumble_backend/AumbleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aumble_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AumbleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
